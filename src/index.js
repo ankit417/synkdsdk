3f258a725e7b02ee97f1e8e84196a4355b8aa5c6
Ankit Acharya <acharya.ankit417@gmail.com>
2022-07-28 17:23:32 +0545

    updated ad format and close button

--- src/index.js
+++ src/index.js
@@ -8,17 +8,18 @@ import {
   StyleSheet,
   Text,
   Linking,
+  Image,
 } from 'react-native';
 import {WebView} from 'react-native-webview';
 
 const DeviceWidth = Dimensions.get('window').width;
 const DeviceHeight = Dimensions.get('window').height;
 
-const URL =
+const MAIN_URL =
   'https://insprep.s3.eu-west-1.amazonaws.com/SDK/apptags/adsdk2.html?';
-const SRC = 'https://media-cdn.synkd.life/fenix.js';
+const MAIN_SRC = 'https://media-cdn.synkd.life/fenix.js';
 
-export const SynkdSdk = ({
+export const SynkdSdkNew = ({
   sKey,
   tag,
   mraid = false,
@@ -127,7 +128,7 @@ const getAdType = (
         CTA,
       );
     default:
-      return defaultAd(
+      return superoptic(
         sKey,
         tag,
         (mraid = false),
@@ -154,52 +155,75 @@ const interStitial = (
 ) => {
   const [modalVisible, setModalVisible] = useState(true);
   const [interstitialHeight, setInterstitialHeight] = useState(DeviceHeight);
-  const [interstitialWidth, setInterstitialWidth] = useState(DeviceWidth);
-
+  const [interstitialWidth, setInterstitialWidth] = useState(DeviceWidth * 0.9);
+  const [imageHeight, setImageHeight] = useState(50);
   const updateAd = () => {
     setModalVisible(!modalVisible);
     setInterstitialHeight(0);
     setInterstitialWidth(0);
   };
+
   return URL ? (
     <View
-      style={[
-        {
-          height: interstitialHeight,
-          width: interstitialWidth,
+      style={{
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 1000,
+        height: interstitialHeight,
+        width: DeviceWidth,
+      }}>
+      <TouchableOpacity
+        onPress={() => {
+          setInterstitialHeight(0);
+        }}
+        style={{
           position: 'absolute',
           top: 0,
-          left: 0,
-        },
-      ]}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
+          padding: 20,
+          zIndex: 1001,
+          right: DeviceWidth / 2,
+          transform: [{translateX: 50}],
+          opacity: interstitialHeight > 0 ? 1 : 0,
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+
+          elevation: 5,
         }}>
-        <TouchableOpacity
-          style={{flex: 1}}
-          onPress={() => Linking.openURL(CTA)}>
-          <WebView
-            style={SDKstyles.adView}
-            originWhitelist={['*']}
-            source={{
-              uri: URL,
-            }}
-            javaScriptEnabled={true}
-            domStorageEnabled={true}
-            startInLoadingState={true}
-            scalesPageToFit={true}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[SDKstyles.button, SDKstyles.buttonClose]}
-          onPress={() => updateAd()}>
-          <Text style={SDKstyles.textStyle}>close</Text>
-        </TouchableOpacity>
-      </Modal>
+        <Text
+          style={{
+            fontSize: 20,
+            borderRadius: 150,
+            borderWidth: 1,
+            padding: 10,
+          }}>
+          X
+        </Text>
+      </TouchableOpacity>
+      <WebView
+        style={{
+          height: '100%',
+          width: '100%',
+        }}
+        originWhitelist={['*']}
+        source={{
+          uri: `${MAIN_URL}src=${MAIN_SRC}&tag=${tag}&key=${sKey}&mraid=${mraid}`,
+        }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        scalesPageToFit={true}
+        onShouldStartLoadWithRequest={(request) => {
+          Linking.openURL(request.url);
+        }}
+      />
     </View>
   ) : (
     <View style={{height: 10}} />
@@ -210,32 +234,68 @@ const SuperOptic = (
   sKey,
   tag,
   mraid = false,
-  height = 400,
-  width = 400,
+  height,
+  width,
   // styles: ViewStyle,
   type,
   URL,
   CTA,
 ) => {
+  const [superOpticHeight, setSuperOpticHeight] = useState(DeviceHeight);
+
   return URL ? (
     <View
       style={[
         {
-          height: DeviceHeight,
+          height: superOpticHeight,
+          width: DeviceWidth * 0.9,
         },
       ]}>
-      <TouchableOpacity style={{flex: 1}} onPress={() => Linking.openURL(CTA)}>
-        <WebView
-          originWhitelist={['*']}
-          source={{
-            uri: URL,
-          }}
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-          startInLoadingState={true}
-          scalesPageToFit={true}
-        />
+      <TouchableOpacity
+        onPress={() => {
+          setSuperOpticHeight(0);
+        }}
+        style={{
+          position: 'absolute',
+          top: 0,
+          padding: 20,
+          zIndex: 1001,
+          right: DeviceWidth / 2,
+          transform: [{translateX: 50}],
+          opacity: superOpticHeight > 0 ? 1 : 0,
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+
+          elevation: 5,
+        }}>
+        <Text
+          style={{
+            fontSize: 20,
+            borderRadius: 150,
+            borderWidth: 1,
+            padding: 10,
+          }}>
+          X
+        </Text>
       </TouchableOpacity>
+      <WebView
+        originWhitelist={['*']}
+        source={{
+          uri: `${MAIN_URL}src=${MAIN_SRC}&tag=${tag}&key=${sKey}&mraid=${mraid}`,
+        }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        scalesPageToFit={true}
+        onShouldStartLoadWithRequest={(request) => {
+          Linking.openURL(request.url);
+        }}
+      />
     </View>
   ) : (
     <View style={{height: 10}} />
@@ -253,21 +313,64 @@ const Sticky = (
   URL,
   CTA,
 ) => {
+  const [stickyHeight, setStickyHeight] = useState(height);
   return URL ? (
-    <View style={{position: 'absolute', bottom: 0, left: 0, right: 0}}>
-      <TouchableOpacity style={{flex: 1,height,width}} onPress={() => Linking.openURL(CTA)}>
-        <WebView
-          style={SDKstyles.adView}
-          originWhitelist={['*']}
-          source={{
-            uri: URL,
-          }}
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-          startInLoadingState={true}
-          scalesPageToFit={true}
-        />
+    <View
+      style={{
+        // position: 'absolute',
+        // bottom: 0,
+        // left: 0,
+        // right: 0,
+        height: stickyHeight,
+        width: DeviceWidth * 0.9,
+      }}>
+      <TouchableOpacity
+        onPress={() => {
+          setStickyHeight(0);
+        }}
+        style={{
+          position: 'absolute',
+          top: 0,
+          padding: 20,
+          zIndex: 1001,
+          right: DeviceWidth / 2,
+          transform: [{translateX: 50}],
+          opacity: stickyHeight > 0 ? 1 : 0,
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+
+          elevation: 5,
+        }}>
+        <Text
+          style={{
+            fontSize: 20,
+            borderRadius: 150,
+            borderWidth: 1,
+            padding: 10,
+          }}>
+          X
+        </Text>
       </TouchableOpacity>
+
+      <WebView
+        // style={SDKstyles.adView}
+        originWhitelist={['*']}
+        source={{
+          uri: `${MAIN_URL}src=${MAIN_SRC}&tag=${tag}&key=${sKey}&mraid=${mraid}`,
+        }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        scalesPageToFit={true}
+        onShouldStartLoadWithRequest={(request) => {
+          Linking.openURL(request.url);
+        }}
+      />
     </View>
   ) : (
     <View style={{height: 10}} />
@@ -295,12 +398,15 @@ const defaultAd = (
         <WebView
           originWhitelist={['*']}
           source={{
-            uri: URL,
+            uri: `${MAIN_URL}src=${MAIN_SRC}&tag=${tag}&key=${sKey}&mraid=${mraid}`,
           }}
           javaScriptEnabled={true}
           domStorageEnabled={true}
           startInLoadingState={true}
           scalesPageToFit={true}
+          onShouldStartLoadWithRequest={(request) => {
+            Linking.openURL(request.url);
+          }}
         />
       </TouchableOpacity>
     </View>
